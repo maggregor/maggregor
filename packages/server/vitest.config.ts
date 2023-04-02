eb864330589cb22ed0be4f536dda84cd0893a4ed
estebgonza <esteban.gonzalez@achilio.com>
2023-04-02 13:22:45 +0200

    chore: add e2e mongo tests

--- packages/server/vitest.config.ts
+++ packages/server/vitest.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vitest/config';
+import AutoImport from 'unplugin-auto-import/vite';
 
 export default defineConfig({
   test: {
-    include: ['src/**/*.spec.ts'],
+    include: ['src/**/*.spec.ts', 'e2e/**/*.test.ts'],
     hookTimeout: 10000,
   },
+  plugins: [
+    AutoImport({
+      imports: ['vitest'],
+      dts: true, // generate TypeScript declaration
+    }),
+  ],
 });
