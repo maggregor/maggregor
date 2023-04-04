ca971a99a5f7a058ff0e35224e73adedc14b2bf0
estebgonza <esteban.gonzalez@achilio.com>
2023-04-04 10:50:20 +0200

    wip

---
+++ packages/webapp/nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "nuxt-vitest",
+    "nuxt-lodash",
+  ],
+});
