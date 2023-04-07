77271cdf09244e568aa06840d1bc06e1b96ce8fd
estebgonza <esteban.gonzalez@achilio.com>
2023-04-07 15:24:16 +0200

    feat: mongodb uri instead port host in the proxy

--- tests/unit/server/modules/mongodb-proxy/proxy.service.test.ts
+++ tests/unit/server/modules/mongodb-proxy/proxy.service.test.ts
@@ -6,8 +6,7 @@ test('should be correctly configured', async () => {
     env: {
       MAGGREGOR_PROXY_HOST: '127.0.0.1',
       MAGGREGOR_PROXY_PORT: 4634,
-      MONGODB_HOST: 'localhost',
-      MONGODB_PORT: 26424,
+      MONGODB_TARGET_URI: 'mongodb://localhost:27017',
     },
   };
   const service = await createProxyServiceTest(opts);
@@ -15,6 +14,30 @@ test('should be correctly configured', async () => {
   expect(service).toBeInstanceOf(MongoDBTcpProxyService);
   expect(service.getProxyHost()).toBe(opts.env.MAGGREGOR_PROXY_HOST);
   expect(service.getProxyPort()).toBe(opts.env.MAGGREGOR_PROXY_PORT);
-  expect(service.getTargetHost()).toBe(opts.env.MONGODB_HOST);
-  expect(service.getTargetPort()).toBe(opts.env.MONGODB_PORT);
+  expect(service.getTargetHost()).toBe('localhost');
+  expect(service.getTargetPort()).toBe(27017);
+});
+
+test('should throw an error if MONGODB_TARGET_URI is invalid', async () => {
+  await expect(
+    createProxyServiceTest({
+      env: {
+        MONGODB_TARGET_URI: 'mongodcalhost',
+      },
+    }),
+  ).rejects.toThrowError();
+});
+
+test('should works with complex MONGODB_TARGET_URI', async () => {
+  const opts = {
+    env: {
+      MONGODB_TARGET_URI:
+        'mongodb+srv://username:password@localhost/name?a=b&c=d',
+    },
+  };
+  const service = await createProxyServiceTest(opts);
+  expect(service).toBeDefined();
+  expect(service).toBeInstanceOf(MongoDBTcpProxyService);
+  expect(service.getTargetHost()).toBe('localhost');
+  expect(service.getTargetPort()).toBe(27017);
 });
