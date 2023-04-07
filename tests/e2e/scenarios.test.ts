e096fe644e4d70167babd8d095ce57744d326e60
estebgonza <esteban.gonzalez@achilio.com>
2023-04-07 22:32:51 +0200

    chore: remove console.log

--- tests/e2e/scenarios.test.ts
+++ tests/e2e/scenarios.test.ts
@@ -3,8 +3,6 @@ import { deepEqual } from 'assert';
 
 test('MongoDB e2e test', async (t) => {
   const client = global.__MONGO_CLIENT__;
-
-  console.log(client);
   for (const scenario of scenarios) {
     const result = await scenario.request(client);
     deepEqual(result, scenario.expectedResponse, scenario.name);
