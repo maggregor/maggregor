fa6a98461839a6528258cc73402443a2768e14fa
estebgonza <esteban.gonzalez@achilio.com>
2023-04-08 19:18:27 +0200

    feat: in-memory cache with frequency strategy

---
+++ src/core/cache.ts
@@ -0,0 +1,80 @@
+import sizeof from 'object-sizeof';
+
+interface CacheItem {
+  result: any;
+  frequency: number;
+  query: string;
+  collection: string;
+  db: string;
+}
+
+export class InMemoryCache {
+  private max_size: number;
+  private cache: Record<string, CacheItem>;
+
+  constructor(max_size_mb: number) {
+    this.max_size = max_size_mb * 1024 * 1024;
+    this.cache = {};
+  }
+
+  get(query: string, collection: string, db: string): any | null {
+    const key = `${query}-${collection}-${db}`;
+    if (key in this.cache) {
+      // Update frequency count
+      this.cache[key].frequency++;
+      return this.cache[key].result;
+    } else {
+      return null;
+    }
+  }
+
+  set(query: string, collection: string, db: string, result: any): void {
+    const key = `${query}-${collection}-${db}`;
+    const item_size = sizeof(result);
+    if (this.currentSize() + item_size > this.max_size) {
+      // Sort items in cache by frequency count
+      const items = Object.keys(this.cache).map((key) => this.cache[key]);
+      items.sort((a, b) => a.frequency - b.frequency);
+      // Evict least frequently used item
+      delete this.cache[
+        `${items[0].query}-${items[0].collection}-${items[0].db}`
+      ];
+    }
+    // Add new item to cache
+    this.cache[key] = {
+      result,
+      frequency: 1,
+      query,
+      collection,
+      db,
+    };
+  }
+
+  private currentSize(): number {
+    // Calculate the current size of the cache
+    let size = 0;
+    for (const key in this.cache) {
+      if (this.cache.hasOwnProperty(key)) {
+        size += sizeof(this.cache[key].result);
+      }
+    }
+    console.log('Current cache size:', size);
+    return size;
+  }
+
+  public invalidateAll(): void {
+    this.cache = {};
+  }
+
+  public invalidateCollection(collection: string, db: string): void {
+    for (const key in this.cache) {
+      if (
+        this.cache.hasOwnProperty(key) &&
+        this.cache[key].collection === collection &&
+        this.cache[key].db === db
+      ) {
+        delete this.cache[key];
+      }
+    }
+  }
+}
---
+++ tests/unit/core/cache.test.ts
@@ -0,0 +1,71 @@
+import { InMemoryCache } from '@core/cache';
+import sizeof from 'object-sizeof';
+
+describe('InMemoryCache', () => {
+  let cache: InMemoryCache;
+
+  beforeEach(() => {
+    cache = new InMemoryCache(10); // 10 MB max size
+  });
+
+  it('should store and retrieve items from cache', () => {
+    const query = 'myquery';
+    const collection = 'users';
+    const db = 'mydb';
+    const result = [{ id: 1, name: 'John Doe' }];
+    cache.set(query, collection, db, result);
+    const cachedResult = cache.get(query, collection, db);
+    expect(cachedResult).to.deep.equal(result);
+  });
+
+  it('should return null for non-existent items', () => {
+    const query = 'myquery';
+    const collection = 'users';
+    const db = 'mydb';
+    const cachedResult = cache.get(query, collection, db);
+    expect(cachedResult).to.be.null;
+  });
+
+  it('should evict least frequently used items when the cache will exceed its max size', () => {
+    const collection = 'users';
+    const db = 'mydb';
+    const result = [{ id: 1, name: 'John Doe' }];
+    const totalSize = sizeof(result) * 3;
+    cache = new InMemoryCache(totalSize / 1024 / 1024); // Convert to MB
+    cache.set('query1', collection, db, result);
+    cache.set('query2', collection, db, result);
+    cache.set('query3', collection, db, result);
+    expect(cache.get('query1', collection, db)).to.not.be.null;
+    expect(cache.get('query2', collection, db)).to.not.be.null;
+    expect(cache.get('query3', collection, db)).to.not.be.null;
+    cache.set('query4', collection, db, result);
+    expect(cache.get('query1', collection, db)).to.be.null;
+    // Simulate a query that is frequently used
+    cache.get('query2', collection, db);
+    cache.get('query3', collection, db);
+    // Add a new item to the cache
+    cache.set('query5', collection, db, result);
+    // Query 4 should have been evicted because it was the least frequently used
+    expect(cache.get('query4', collection, db)).to.be.null;
+  });
+
+  it("should invalidate all items in the cache when 'clear' is called", () => {
+    const query = 'myquery';
+    const collection = 'users';
+    const db = 'mydb';
+    const result = [{ id: 1, name: 'John Doe' }];
+    cache.set(query, collection, db, result);
+    cache.invalidateAll();
+    expect(cache.get(query, collection, db)).to.be.null;
+  });
+
+  it("should invalidate a specific collection in the cache when 'invalidateCollection' is called", () => {
+    const query = 'myquery';
+    const collection = 'users';
+    const db = 'mydb';
+    const result = [{ id: 1, name: 'John Doe' }];
+    cache.set(query, collection, db, result);
+    cache.invalidateCollection(collection, db);
+    expect(cache.get(query, collection, db)).to.be.null;
+  });
+});
