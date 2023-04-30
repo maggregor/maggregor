aa9b9dd4973d6ddb2eeb3c3e021954417de7c924
estebgonza <esteban.gonzalez@achilio.com>
2023-04-30 21:09:02 +0200

    merge

--- benchmarks/count.scenario.ts
+++ benchmarks/count.scenario.ts
@@ -1,11 +1,11 @@
 import { MaggregorBenchmarkScenario } from 'benchmarks';
 import { MongoClient } from 'mongodb';
 
+// Maggregor doesn't improve the performance of count
+// No expected speed
 const scenario: MaggregorBenchmarkScenario = {
   name: 'count',
   description: 'count all and counter new york users | no changes',
-  // Maggregor doesn't improve the performance of count
-  // expectedSpeedTreshold: 0,
   run: async (client: MongoClient, db: string, collection: string) => {
     const req1 = client.db(db).collection(collection).count();
     const req2 = client
--- benchmarks/distinct.scenario.ts
+++ benchmarks/distinct.scenario.ts
@@ -1,11 +1,11 @@
 import { MaggregorBenchmarkScenario } from 'benchmarks';
 import { MongoClient } from 'mongodb';
 
+// Maggregor doesn't improve the performance of distinct
+// No expected speed
 const scenario: MaggregorBenchmarkScenario = {
   name: 'distinct',
   description: 'distinct city | no changes',
-  // Maggregor doesn't improve the performance of distinct
-  // expectedSpeedTreshold: 0,
   run: async (client: MongoClient, db: string, collection: string) => {
     await client.db(db).collection(collection).distinct('city');
   },
